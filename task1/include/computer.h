7f1d32cdc074181e67575324a10ced1989b3c566
learningongb <mnetckachev@yandex.ru>
2024-05-24 00:00:46 +0300

    поправил алгоритм расчета хода компьютера

--- task1/include/computer.h
+++ task1/include/computer.h
@@ -20,13 +20,13 @@ public:
         {
             result = 1;
         } 
-        else if (max_count_per_move >= count)
+        else if (max_count_per_move +1 >= count)
         {
             result = count - 1;
         }
         else
         {
-            int best_count = (count / (max_count_per_move + 1)) * (max_count_per_move + 1);
+            int best_count = ((count - 1)/ (max_count_per_move + 1)) * (max_count_per_move + 1) + 1;
             if (count > best_count)
             {
                 result = count - best_count;
